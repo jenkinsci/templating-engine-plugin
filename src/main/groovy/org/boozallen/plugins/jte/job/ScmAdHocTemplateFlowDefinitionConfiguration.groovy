4001d1b5a6f81669e969414287f2df0b9b9488fa
steven-terrana <steventerrana@gmail.com>
2020-11-05 14:00:12 -0500

    add scm scaffold

---
+++ src/main/groovy/org/boozallen/plugins/jte/job/ScmAdHocTemplateFlowDefinitionConfiguration.groovy
@@ -0,0 +1,67 @@
+/*
+    Copyright 2018 Booz Allen Hamilton
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+package org.boozallen.plugins.jte.job
+
+import hudson.Extension
+import hudson.Util
+import hudson.scm.SCM
+import org.boozallen.plugins.jte.init.governance.config.dsl.PipelineConfigurationDsl
+import org.boozallen.plugins.jte.init.governance.config.dsl.PipelineConfigurationObject
+import org.boozallen.plugins.jte.util.TemplateLogger
+import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner
+import org.kohsuke.stapler.DataBoundConstructor
+
+/**
+ * Defines a pipeline configuration repository from the Jenkins console.
+ * <p>
+ * Allows users to define a pipeline configuration, default pipeline template, and named
+ * pipeline templates via a pipeline catalog.
+ */
+class ScmAdHocTemplateFlowDefinitionConfiguration extends AdHocTemplateFlowDefinitionConfiguration{
+
+    SCM scm
+    String pipelineConfigurationPath
+    String pipelineTemplatePath
+
+    @DataBoundConstructor
+    ScmAdHocTemplateFlowDefinitionConfiguration(SCM scm, String pipelineConfigurationPath, String pipelineTemplatePath){
+        this.scm = scm
+        this.pipelineConfigurationPath = Util.fixEmptyAndTrim(pipelineConfigurationPath)
+        this.pipelineTemplatePath = Util.fixEmptyAndTrim(pipelineTemplatePath)
+    }
+
+    @Override
+    Boolean hasConfig(FlowExecutionOwner flowOwner){}
+
+    @Override
+    PipelineConfigurationObject getConfig(FlowExecutionOwner flowOwner) throws Exception{}
+
+    @Override
+    Boolean hasTemplate(FlowExecutionOwner flowOwner){}
+
+    @Override
+    String getTemplate(FlowExecutionOwner owner){}
+
+    @Extension
+    static class DescriptorImpl extends AdHocTemplateFlowDefinitionConfiguration.DescriptorImpl {
+
+        String getDisplayName(){
+            return "From SCM"
+        }
+
+    }
+
+}
