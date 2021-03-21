5f2901745d39280b4e8add9f989a73a925daa72e
Steven Terrana <steventerrana@gmail.com>
2021-03-21 08:55:30 -0400

    remove TemplatePrimitive, switch to GlobalVariables

---
+++ src/main/groovy/org/boozallen/plugins/jte/init/primitives/NamespaceCollector.groovy
@@ -0,0 +1,4 @@
+package org.boozallen.plugins.jte.init.primitives
+
+class NamespaceCollector {
+}
---
+++ src/main/groovy/org/boozallen/plugins/jte/init/primitives/TemplatePrimitiveGV.groovy
@@ -0,0 +1,5 @@
+package org.boozallen.plugins.jte.init.primitives
+
+import org.jenkinsci.plugins.workflow.cps.GlobalVariable
+
+abstract class GlobalVariableTemplatePrimitive extends GlobalVariable{}
