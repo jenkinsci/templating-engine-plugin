1a702e67ec3c1119da0bcd5f8601699d89f53c2c
Steven Terrana <steventerrana@gmail.com>
2022-05-25 09:20:08 -0400

    fix

--- src/test/groovy/org/boozallen/plugins/jte/ReplaySpec.groovy
+++ src/test/groovy/org/boozallen/plugins/jte/ReplaySpec.groovy
@@ -1,3 +1,18 @@
+/*
+    Copyright 2018 Booz Allen Hamilton
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 package org.boozallen.plugins.jte
 
 import org.boozallen.plugins.jte.util.TestUtil
@@ -39,7 +54,7 @@ class ReplaySpec extends Specification{
         then:
         WorkflowRun b2 = b1.getAction(ReplayAction).run(template, [:]).get()
         jenkins.assertBuildStatusSuccess(b2)
-        jenkins.assertLogContains("hello world")
-
+        jenkins.assertLogContains("hello world", b2)
     }
+
 }
